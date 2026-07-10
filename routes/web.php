6ce4e5c89dd139210202aa1fdb206a48b358e5d3
Kemal <kemalpolatkemal1@gmail.com>
2026-07-10 10:58:14 +0300

    Log in log out ve admin routes

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AuthController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::prefix('admin')->group(function () {
-    Route::get('/', [AdminController::class, 'index'])->name('admin.dashboard');
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::middleware('auth')->group(function () {
+
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.dashboard');
 });
